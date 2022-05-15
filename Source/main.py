84b9a068dbc260d954ca2f7981354876ec0796b7
Walther Medina Pauca <wmedinap@ulasalle.edu.pe>
2022-05-15 17:07:36 -0500

    Update main
    Se implementa la funcion para poder desarrollar los trapecios y de esta forma hallar la integral.

--- Source/main.py
+++ Source/main.py
@@ -1,6 +1,8 @@
 import math
 import numpy as np
 from matplotlib import pyplot as plt
+import threading
+import time
 
 x = 0
 
@@ -9,6 +11,29 @@ def funcion(x):
     return y
 
 r = np.linspace(0,10,100)
-plt.ion()
+#plt.ion()
 plt.plot(r,funcion(r))
+
+
+b = float(input('Ingrese el limite superior: '))
+a = float(input('Ingrese el limite inferior: '))
+n_trapecios = int(input('Ingrese el numero de trapecios: '))
+
+x = np.zeros([n_trapecios + 1])
+
+h = (b - a) / n_trapecios
+x[0] = a
+x[n_trapecios] = b
+suma = 0
+
+
+for i in np.arange(1, n_trapecios):
+    x[i] = x[i-1] + h
+    suma = suma + funcion(x[i])
+
+
+integral = (h / 2) * (funcion(x[0]) + 2 * suma + funcion(x[n_trapecios]))
+
+print("Resultado: ", round(integral, 10))
+
 
